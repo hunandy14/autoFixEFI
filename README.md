8fe7e3800d8f7b238812a406f9c7a971e9bf6769
Charlotte.Hong <hunandy14@gmail.com>
2022-01-15 19:12:34 +0900

    新增MBR修復

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-自動修復EFI啟動分區
+自動修復 Windows 啟動分區
 ===
 
+## EFI
 輸入需要修復的 Windows 資料夾槽，程序會自動修復該磁碟中的EFI分區。  
   
 下面指令預設的槽位是K槽，將硬碟接上電腦後請自行修改成對應的槽位。  
@@ -15,8 +16,18 @@ autoFixEFI -DriveLetter:K
 短網址指令
 
 ```
-irm bit.ly/3EQwFzs|iex; autoFixEFI "K"
+irm bit.ly/3EQwFzs|iex; autoFixEFI K
+```
+
+## MBR
+```
+irm "https://raw.githubusercontent.com/hunandy14/autoFixEFI/master/autoFixMBR.ps1" | iex
+autoFixMBR -DriveLetter:K
+```
+
+```
+irm bit.ly/3rk6Jrk|iex; autoFixMBR K
 ```
 
 ## 注意事項
-預設會把EFI分區掛載到 X 曹位，如果該曹位已經被占用了，還沒寫防呆會直接報錯失敗。
+預設會把啟動分區掛載到 X 曹位，如果該曹位已經被占用了，還沒寫防呆會直接報錯失敗。
---
+++ autoFixMBR.ps1
@@ -0,0 +1,40 @@
+function autoFixMBR {
+    param (
+        [Parameter(Position = 0, ParameterSetName = "", Mandatory=$true)]
+        [string] $DriveLetter
+    )
+    $Dri=(Get-Partition -DriveLetter:$DriveLetter)
+    if (!$Dri) { Write-Host "錯誤::請輸入正確的磁碟代號"; return}
+    if (($Dri|Get-Disk).PartitionStyle -ne "MBR") {
+        Write-Host "錯誤::該分區的磁碟為 MBR 非 GPT 格式"; return }
+    
+    $MBR_Letter  = "X"
+    $Active = $Dri|Where-Object{$_.IsActive}
+    
+    if (!$Active) {
+        Write-Host "該磁碟沒有啟動分區，即將把" -NoNewline
+        Write-Host " ($($DriveLetter):) " -ForegroundColor:Yellow -NoNewline
+        Write-Host "設置成啟動分區" 
+        $response = Read-Host "  沒有異議或看不懂，請輸入Y (Y/N) ";
+        if ($response -ne "Y" -or $response -ne "Y") { Write-Host "使用者中斷" -ForegroundColor:Red; return; }
+        $Dri|Set-Partition -IsActive $True
+        $Active = $Dri|Where-Object{$_.IsActive}
+    }
+    Get-Partition -DiskNumber:$Dri.DiskNumber | Out-Default 
+    Write-Host "即將把" -NoNewline
+    Write-Host " ($($DriveLetter):\windows) " -ForegroundColor:Yellow -NoNewline
+    Write-Host "的啟動引導, " -NoNewline
+    Write-Host "寫入MBR引導分區" -NoNewline
+    Write-Host " (磁碟:$($Active.DiskNumber), 分區:$($Active.PartitionNumber)) " -ForegroundColor:Yellow
+    $response = Read-Host "  沒有異議或看不懂，請輸入Y (Y/N) "
+    if (($response -ne "Y") -or ($response -ne "Y")) { Write-Host "使用者中斷" -ForegroundColor:Red; return; }
+    # 新增Active磁碟代號
+    if(!$Active.DriveLetter){ $Active|Set-Partition -NewDriveLetter:$MBR_Letter; $Active=$Active|Get-Partition; }
+    # 重建MBR開機引導
+    $cmd = "bcdboot $($DriveLetter):\windows /f BIOS /s $($EFI_Letter):\ /l zh-tw"
+    Invoke-Expression $cmd
+    # 移除EFI磁碟代號
+    if ($DriveLetter -ne $Active.DriveLetter) {
+        $Active|Remove-PartitionAccessPath -AccessPath:"$($Active.DriveLetter):"
+    }
+} # autoFixMBR C
