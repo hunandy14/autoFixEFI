eaa6e77dc2c4b0a4e54668248b48359f8f683885
Charlotte.Hong <hunandy14@gmail.com>
2022-01-23 22:52:49 +0900

    修復錯誤

--- autoFixBoot.ps1
+++ autoFixBoot.ps1
@@ -1,4 +1,4 @@
-
+# 自動修復 Windows 分區
 function autoFixBoot {
     param (
         [Parameter(Position = 0, ParameterSetName = "", Mandatory=$true)]
@@ -9,11 +9,11 @@ function autoFixBoot {
     if (!$Dri) { Write-Host "錯誤::請輸入正確的磁碟代號"; return}
     # 修復引導
     $Boot = ($Dri|Get-Disk).PartitionStyle
-    if ($Boot -ne "GPT") {
+    if ($Boot -eq "GPT") {
         irm "https://raw.githubusercontent.com/hunandy14/autoFixEFI/master/autoFixEFI.ps1"|iex
         autoFixEFI -DriveLetter:$DriveLetter -Force:$Force
-    } elseif ($Boot -ne "MBR") { 
+    } elseif ($Boot -eq "MBR") { 
         irm "https://raw.githubusercontent.com/hunandy14/autoFixEFI/master/autoFixMBR.ps1"|iex
         autoFixMBR -DriveLetter:$DriveLetter -Force:$Force
     }
-}
+} # autoFixBoot C
