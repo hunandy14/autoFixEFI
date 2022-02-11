d225293486313fbc02067a7b6508777961472cdd
Charlotte.Hong <hunandy14@gmail.com>
2022-02-11 10:50:15 +0900

    修正容量問題，EFI分區系統預設有保留一段空間

--- autoFixEFI.ps1
+++ autoFixEFI.ps1
@@ -10,7 +10,7 @@ function autoFixEFI {
     if (($Dri|Get-Disk).PartitionStyle -ne "GPT") {
         Write-Host "錯誤::該分區的磁碟為 MBR 非 GPT 格式"; return }
     
-    $EfiSize     = (300MB + 8)
+    $EfiSize     = 300MB
     $response    = ""
     $DriveLetter = $DriveLetter.Trim(":")
     $EFI_ID      = "{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}"
@@ -29,7 +29,21 @@ function autoFixEFI {
             $response = Read-Host "  沒有異議或看不懂，請輸入Y (Y/N) ";
             if ($response -ne "Y" -or $response -ne "Y") { Write-Host "使用者中斷" -ForegroundColor:Red; return; }
         }
-        $Dri|Resize-Partition -Size:($Dri.size-$EfiSize)
+        # 查詢與計算空間
+        $SupSize = $Dri|Get-PartitionSupportedSize
+        $CurSize = $Dri.size
+        $MaxSize = $SupSize.SizeMax
+        $AvailableSize = $MaxSize - $CurSize
+        # 空餘空間小於EFI空間，壓縮C曹
+        if ($AvailableSize -lt $EfiSize) {
+            $ReSize = $MaxSize - $EfiSize
+            # 目標分區在結尾(GPT結尾必須保留 1048576 空間)
+            if ((($Dri|Get-Disk|Get-Partition)[-1]).UniqueId -eq $Dri.UniqueId) {
+                $ReSize = $ReSize - 1048576
+            }
+            $Dri|Resize-Partition -Size:$ReSize
+        }
+        # 建立EFI分區並格式化
         $EFI = (($Dri|New-Partition -Size:($EfiSize) -GptType:$EFI_ID)|Format-Volume -FileSystem:FAT32 -Force)|Get-Partition
         
     }
@@ -53,4 +67,4 @@ function autoFixEFI {
     # 移除EFI磁碟代號
     $EFI|Remove-PartitionAccessPath -AccessPath:"$($EFI.DriveLetter):"
 }
-# autoFixEFI -DriveLetter:T
+# autoFixEFI -DriveLetter:C -Force
