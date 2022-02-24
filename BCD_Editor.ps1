a17f9342fce455e69a7705cfa007c5e55deb43c1
Charlotte.Hong <hunandy14@gmail.com>
2022-02-24 23:47:28 +0900

    新版 GET-BCD 動態反射把 BCD 的 Bean 做出來了

--- BCD_Editor.ps1
+++ BCD_Editor.ps1
@@ -1,40 +1,127 @@
-function __BootList__ {
+# 獲取BCD物件
+function Get-BCD {
     param (
         
     )
+    $BCD_Object = @()
     # 解析 BCD
-    $BCD_Str = (bcdedit)
-    $BCD = $BCD_Str.Split("`n")
-    $BootCount = ($BCD|Select-String -AllMatches "identifier").Count - 1
-    $BootHead = ($BCD)[3..13]
-    
-    $obj = $BCD_Str | Select-String -Pattern "identifier"
+    $BCD = (bcdedit).Split("`n")
+    $BootCount = ($BCD|Select-String -AllMatches "identifier").Count-1
+    $obj = $BCD | Select-String -Pattern "identifier"
+
     # 計算 Loder 屬性數量
     if($obj.Length -le 1){
         Write-Host "No Loder"; return
     } elseif ($obj.Length -eq 2) {
         $AttrCount = $BCD.Count - $obj[1].LineNumber +1
+        $HeadCount = $obj[1].LineNumber-7
+        $Offset = $obj[1].LineNumber-1
     } else {
         $AttrCount = $obj[2].LineNumber - $obj[1].LineNumber -3
+        $HeadCount = $BCD.Count - $obj[1].LineNumber +1
+        $Offset = $obj[1].LineNumber-1
     }
+
+    # 解析 BCD 表頭
+    # $BootHeader = @()
+    $Item = @{}
+    for ($i = 0; $i -lt $HeadCount; $i++) {
+        $Line = $BCD[3+$i]
+        $Attr = $Line.Substring(0,24).trim()
+        $Value = $Line.Substring(24,$Line.Length-24).trim()
+        $Item += @{ $Attr = $Value}
+    } $Item = $Item|ForEach-Object { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+    # $BootHeader += @($Item)
+    $BCD_Object += @($Item)
+
     # 解析 BCD 選單
-    $BootList = @()
-    for ($i = 0; $i -lt $BootCount; $i++) {
-        $Star = ($i*$BootCount)+16
-        $Item = @{ Number = $i+1}
-        for ($j = 0; $j -lt $AttrCount; $j++) {
-            $idx = $Star+$j
-            $Line =  $BCD[$idx]
+    # $BootLoader = @()
+    for ($j = 0; $j -lt $BootCount; $j++) {
+        $Star = ($j*$BootCount)+$Offset
+        $Item = @{Number = $j+1}
+        for ($i = 0; $i -lt $AttrCount; $i++) {
+            $Line = $BCD[$Star+$i]
             $Attr = $Line.Substring(0,24).trim()
             $Value = $Line.Substring(24,$Line.Length-24).trim()
             $Item += @{ $Attr = $Value}
-        } 
-        $Item = $Item|ForEach-Object { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
-        # __Boot_Print__ $Item
-        $BootList += @($Item)
+        } $Item = $Item|ForEach-Object { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+        # $BootLoader += @($Item)
+        $BCD_Object += @($Item)
     }
-    return $BootList
-} # (__BootList__)|Format-Table Number,description,@{Name='Letter'; Expression={$_.device -replace"partition=", ""}},resumeobject
+    # 改變預設輸出模式
+    $DefaultProps = @('Number','description','device','resumeobject')
+    $DefaultDisplay = New-Object System.Management.Automation.PSPropertySet("DefaultDisplayPropertySet",[string[]]$DefaultProps)
+    $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($DefaultDisplay)
+    $BCD_Object | Add-Member MemberSet PSStandardMembers $PSStandardMembers
+    
+    return $BCD_Object
+} # Get-BCD
+function __Get-BCD_Tester__ {
+    param (
+        
+    )
+    # 直接輸出看結果
+    # Get-BCD
+    # 測試RE環境
+    # (Get-BCD)[1].recoveryenabled
+    # 獲取預設選單
+    # $default = (Get-BCD)|Where-Object{$_.identifier -contains ((Get-BCD)[0].default)}
+    # $default|Format-Table Number,description,@{Name='Letter'; Expression={$_.device -replace"partition=", ""}},resumeobject
+    # 獲取當前系統
+    # $current = (Get-BCD)|Where-Object{$_.identifier -contains '{current}'}
+    # $current|Format-Table Number,description,@{Name='Letter'; Expression={$_.device -replace"partition=", ""}},resumeobject
+} # __Get-BCD_Tester__
+
+function __BootList__ {
+    param (
+        
+    )
+    # 解析 BCD
+    $BCD = (bcdedit).Split("`n")
+    $BootCount = ($BCD|Select-String -AllMatches "identifier").Count-1
+    $obj = $BCD | Select-String -Pattern "identifier"
+
+    # 計算 Loder 屬性數量
+    if($obj.Length -le 1){
+        Write-Host "No Loder"; return
+    } elseif ($obj.Length -eq 2) {
+        $AttrCount = $BCD.Count - $obj[1].LineNumber +1
+        $HeadCount = $obj[1].LineNumber-7
+        $Offset = $obj[1].LineNumber-1
+    } else {
+        $AttrCount = $obj[2].LineNumber - $obj[1].LineNumber -3
+        $HeadCount = $BCD.Count - $obj[1].LineNumber +1
+        $Offset = $obj[1].LineNumber-1
+    }
+
+    # 解析 BCD 表頭
+    $BootHeader = @()
+    $Item = @{}
+    for ($i = 0; $i -lt $HeadCount; $i++) {
+        $Line = $BCD[3+$i]
+        $Attr = $Line.Substring(0,24).trim()
+        $Value = $Line.Substring(24,$Line.Length-24).trim()
+        $Item += @{ $Attr = $Value}
+    } $Item = $Item|ForEach-Object { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+    $BootHeader += @($Item)
+
+    # 解析 BCD 選單
+    $BootLoader = @()
+    for ($j = 0; $j -lt $BootCount; $j++) {
+        $Star = ($j*$BootCount)+$Offset
+        $Item = @{Number = $i+1}
+        for ($i = 0; $i -lt $AttrCount; $i++) {
+            $Line = $BCD[$Star+$i]
+            $Attr = $Line.Substring(0,24).trim()
+            $Value = $Line.Substring(24,$Line.Length-24).trim()
+            $Item += @{ $Attr = $Value}
+        } $Item = $Item|ForEach-Object { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+        $BootLoader += @($Item)
+    }
+    # return $BootLoader
+} 
+(__BootList__)
+#  (__BootList__)|Format-Table Number,description,@{Name='Letter'; Expression={$_.device -replace"partition=", ""}},resumeobject
 
 function __Boot_Print__ {
     param (
@@ -45,7 +132,7 @@ function __Boot_Print__ {
 } # __Boot_Print__
 
 
-__Boot_Print__ (__BootList__)
+# __Boot_Print__ (__BootList__)
 
 
 
